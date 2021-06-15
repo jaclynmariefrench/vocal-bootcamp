409e9c119c966907a0cdf6cdc6e658c46e9d1a40
Jaclyn French <jaclynmariefrench@gmail.com>
2021-06-15 10:41:25 -0500

    got widget to render

---
+++ src/components/audioPlayer/AudioProvider.js
@@ -0,0 +1,15 @@
+import React, { useState, createContext } from "react"
+
+// export const AudioContext = createContext()
+
+// export const AudioProvider = () => {
+// [trackType, setTracks] = useState([])
+// [track, playlist] = useState([])
+
+// const getTracks = () => {
+//     return fetch("")
+//     .then(res => res.json())
+//     .then(setTracks)
+// }
+
+// }
