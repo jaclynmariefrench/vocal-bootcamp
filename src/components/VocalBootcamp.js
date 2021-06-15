0ba3ceaa71ee47f92880f77143d351e0752b387d
Jaclyn French <jaclynmariefrench@gmail.com>
2021-06-15 09:39:42 -0500

    i have nav bar set up and page concepts layed out

--- src/components/VocalBootcamp.js
+++ src/components/VocalBootcamp.js
@@ -2,11 +2,12 @@ import React from "react";
 import { ApplicationViews } from "./ApplicationViews";
 import { NavBar } from "./nav/NavBar";
 import { Route } from "react-router-dom";
+import "./VocalBootcamp.css"
 
 export const VocalBootcamp = () => {
     return (
         <>
-        <h1>HEY</h1>
+        <h1>VOCAL BOOTCAMP</h1>
         <Route path="/">
             <NavBar/>
             <ApplicationViews/>
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -5,16 +5,15 @@ import "./NavBar.css"
 export const NavBar = (props) => {
     return (
         <ul className="navbar">
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/">logout</Link>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/home">home</Link>
             </li>
             <li className="navbar__item">
                 <Link className="navbar__link" to="/goals">goals</Link>
             </li>
-            <li className="navbar__item">
-                <Link className="navbar__link" to="/home">home</Link>
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/">logout</Link>
             </li>
-            
         </ul>
     )
 }
